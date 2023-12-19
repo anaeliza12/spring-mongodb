6cf3204fbbdc500e849b9f180a91d4b79251985f
Ana Eliza <anaelizaperobelli@gmail.com>
2023-12-19 19:43:17 -0300

    Comment entity creation

---
+++ src/main/java/com/ana/workshopmongodb/entities/Comment.java
@@ -0,0 +1,83 @@
+package com.ana.workshopmongodb.entities;
+
+import java.util.Date;
+import java.util.Objects;
+
+public class Comment {
+
+	private String id;
+	private String text;
+	private Date date;
+	private Post post;
+	private User user;
+
+	public Comment() {
+
+	}
+
+	public Comment(String id, String text, Date date, Post post, User user) {
+		this.id = id;
+		this.text = text;
+		this.date = date;
+		this.post = post;
+		this.user = user;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public Post getPost() {
+		return post;
+	}
+
+	public void setPost(Post post) {
+		this.post = post;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Comment other = (Comment) obj;
+		return Objects.equals(id, other.id);
+	}
+
+}
